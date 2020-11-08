29cf35cb80c39982957d785e7e7c98880c8f517a
Hakan Bilgin <hbi99@hotmail.com>
2020-11-08 16:13:45 +0100

    gitignore + license

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+const spotify = {
+	init() {
+		// fast references
+		this.content = window.find("content");
+	},
+	dispatch(event) {
+		switch (event.type) {
+			case "window.open":
+				break;
+			case "open-help":
+				defiant.shell("fs -u '~/help/index.md'");
+				break;
+		}
+	}
+};
+
+window.exports = spotify;
