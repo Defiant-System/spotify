9c401795ffb803f1aa76aaf8ddca72469052a951
Hakan Bilgin <hbi99@hotmail.com>
2021-01-06 01:33:11 +0100

    ui + ux

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,28 @@ const spotify = {
 		this.content = window.find("content");
 	},
 	dispatch(event) {
+		let Self = spotify,
+			el;
 		switch (event.type) {
 			case "window.open":
 				break;
 			case "open-help":
 				defiant.shell("fs -u '~/help/index.md'");
+				break;
+			case "do-sidebar-list":
+				el = $(event.target);
+				if (el.hasClass("folder")) {
+					el.toggleClass("expanded", el.hasClass("expanded"));
+				} else if (el.hasClass("item")) {
+					el.parents(".sidebar").find(".active").removeClass("active");
+					el.addClass("active");
+				}
+				break;
+			case "go-to-home":
+				Self.content.find(".sidebar").addClass("show");
+				break;
+			case "go-to-browse":
+				Self.content.find(".sidebar").removeClass("show");
 				break;
 		}
 	}
